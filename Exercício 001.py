c7da7e6e65735cfc8f7c21fac63039609d0fd03b
Amanda <56207130+amandazevedo01@users.noreply.github.com>
2021-11-02 20:58:02 -0300

    Add files via upload

---
+++ Exercício 001.py
@@ -0,0 +1,15 @@
+"""
+EXERCÍCIO 001: Olá, Mundo!
+
+Crie um programa que escreva "Olá, Mundo!" na tela.
+"""
+msg = 'Olá, Mundo!'
+print(msg)
+print(1+2)
+a=int(input("Digite um numero"))
+b=int(input("Digite outro numero vacilao"))
+print(a)
+if a>b:
+    print(f"é maior que {b}")
+else:
+    print(f"não é maior que{b}")
